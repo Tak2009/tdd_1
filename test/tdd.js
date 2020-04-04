9b773d559a4e780ffba858d940465f4802337fd7
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-04-04 15:53:29 +0100

    changed

--- test/tdd.js
+++ test/tdd.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const math = require('../math.js')
 
-//// TDD suiteは　BDD interfaceではエラーになる chaeck pachage json
+//// TDD suiteは UI TDD の時のみ check script
 suite('計算確認　with assert', ()=> {
     test('掛け算 assert', () => {
       assert.equal(6, math.multi(2, 3), '戻り値は6');
@@ -11,9 +11,16 @@ suite('計算確認　with assert', ()=> {
     });
 });
 
-// TDD with no message
+//// TDD with no message
 assert.equal(math.multi(2,  3), 6, '戻り値は6');
 assert.equal(math.div(6,  3), 2, '戻り値は2');
+
+
+////TDD with message 
+suite('モジュール内のオブジェクトの確認 with assert', () =>{
+    test('配列かどうかの確認')
+})
+
 
 
 //// TDD with no message
